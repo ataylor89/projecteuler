17311beabf677f76007961bd5f9e2741c775cdee
Andrew Taylor <rolyata47@gmail.com>
2015-03-20 13:30:18 -0500

    Problem 1

---
+++ src/problems/Problem1.java
@@ -0,0 +1,23 @@
+package problems;
+
+/**
+ *
+ * @author ataylor
+ */
+public class Problem1 {
+    
+    public void solve() {
+        int sum = 0;
+        
+        for (int n = 3; n < 1000; n++) {
+            if (n % 3 == 0 || n % 5 == 0)
+                sum += n;
+        }
+        
+        System.out.println(sum);
+    }
+    
+    public static void main(String[] args) {
+        new Problem1().solve();
+    }
+}
