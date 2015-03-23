c53e4e61eb4f711d35cf07b2845f3af7c98e012c
Andrew Taylor <rolyata47@gmail.com>
2015-03-23 14:23:39 -0500

    Tidying

--- src/problems/Problem14.java
+++ src/problems/Problem14.java
@@ -16,8 +16,6 @@ public class Problem14 {
             
             if (seq.length() > longest.length())
                 longest = seq;
-            
-            System.out.println(n);
         }
         
         System.out.println("Start: " + longest.get(0) + ", Length: " + longest.length());
