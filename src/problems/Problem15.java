1d7d90af91abf59c97243ad8efce3c3d248e81bb
Andrew Taylor <rolyata47@gmail.com>
2015-03-23 15:47:59 -0500

    Finished problem 15, but could use improvement.

--- src/problems/Problem15.java
+++ src/problems/Problem15.java
@@ -1,44 +1,57 @@
 package problems;
 
+import java.util.Arrays;
+
 /**
  *
  * @author ataylor
  */
 public class Problem15 {
 
-    long numPaths = 0;
-    final long SIZE_OF_GRID;
-
-    public Problem15(int sizeOfGrid) {
-        SIZE_OF_GRID = sizeOfGrid;
+    private long[] paths = new long[21];
+    
+    public void solve() {
+        Arrays.fill(paths, 0);
+        
+        for (int size = 1; size <= 20; size++) 
+            solve(size, 0, 0);
+        
+        System.out.println(paths[20]);
     }
-
-    public void step(int xPos, int yPos) {
-        if (xPos == SIZE_OF_GRID && yPos == SIZE_OF_GRID) {
-            numPaths++;
-            if (numPaths % 100000 == 0) {
-                System.out.printf("Paths found: %,d\n", numPaths);
-            }
-        }
-        if (xPos < SIZE_OF_GRID) {
-            step(xPos + 1, yPos);
-        }
-        if (yPos < SIZE_OF_GRID) {
-            step(xPos, yPos + 1);
+    
+    private void solve(int grid, int x, int y) {
+        if (isFinished(grid, x, y))
+            paths[grid] += 1;
+        
+        else if (isKnown(grid, x, y))
+           paths[grid] += paths[grid-x];
+        
+        else {
+            if (hasRight(grid, x, y))
+                solve(grid, x+1, y);
+            
+            if (hasDown(grid, x, y))
+                solve(grid, x, y+1);
         }
     }
-
-    public static void main(String[] args) throws Exception {
-        if (args.length != 1) {
-            System.err.println("Usage: java Problem15 <size_of_grid>");
-            System.exit(1);
-        }
-
-        int sizeOfGrid = Integer.parseInt(args[0]);
-        Problem15 p = new Problem15(sizeOfGrid);
-
-        p.step(1, 0);
-        p.step(0, 1);
-        System.out.println("Number of paths: " + p.numPaths);
+    
+    private boolean isFinished(int grid, int x, int y) {
+        return x == grid && y == grid;
+    }
+    
+    private boolean isKnown(int grid, int x, int y) {
+        return (x == y) && (paths[grid-x] != 0);
+    }
+    
+    private boolean hasRight(int grid, int x, int y) {
+        return y <= grid && x < grid;
+    }
+    
+    private boolean hasDown(int grid, int x, int y) {
+        return x <= grid && y < grid;
+    }
+    
+    public static void main(String[] args) {
+        new Problem15().solve();
     }
 }
