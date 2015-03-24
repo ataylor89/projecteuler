a2f418442d40ad08a74c4b2add3a774b54b73067
Andrew Taylor <rolyata47@gmail.com>
2015-03-24 10:27:59 -0500

    Better solution for problem 15

--- src/problems/problem15/Solution1.java
+++ src/problems/problem15/Solution1.java
@@ -1,4 +1,4 @@
-package problems;
+package problems.problem15;
 
 import java.util.Arrays;
 
@@ -6,7 +6,7 @@ import java.util.Arrays;
  *
  * @author ataylor
  */
-public class Problem15 {
+public class Solution1 {
 
     private long[] paths = new long[21];
     
@@ -52,6 +52,6 @@ public class Problem15 {
     }
     
     public static void main(String[] args) {
-        new Problem15().solve();
+        new Solution1().solve();
     }
 }
---
+++ src/problems/problem15/Solution2.java
@@ -0,0 +1,39 @@
+package problems.problem15;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author ataylor
+ */
+public class Solution2 {
+    
+    public void solve() {
+        long begin = System.currentTimeMillis();
+        long answer = solve(new long[21][21], 0, 0);
+
+        long runningTime = System.currentTimeMillis() - begin;
+        System.out.println("Number of paths: " + answer);
+        System.out.println("Execution time: " + runningTime + "ms.");
+    }
+    
+    private long solve(long[][] grid, int x, int y) {
+        int end = grid.length-1;
+        
+        if (x > end || y > end)
+            return 0;
+        
+        if (x == end && y == end)
+            return 1;
+        
+        if (grid[x][y] != 0)
+            return grid[x][y];
+        
+        grid[x][y] = solve(grid, x + 1, y) + solve(grid, x, y + 1);
+        return grid[x][y];
+    }
+    
+    public static void main(String[] args) {
+        new Solution2().solve();
+    }
+}
