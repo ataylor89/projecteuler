a639e2873fd4a555afaee63d6ca8680c65b4a8c8
Andrew Taylor <rolyata47@gmail.com>
2015-03-27 15:28:52 -0500

    Finished problem 46

--- src/problems/Problem46.java
+++ src/problems/Problem46.java
@@ -1,70 +1,47 @@
 package problems;
 
+import java.util.List;
+import util.SieveOfEratosthenes;
+
 /**
  *
  * @author ataylor
  */
 public class Problem46 {
+
+    private SieveOfEratosthenes sieve;
     
-    private final int MAX = 10000;
-    private boolean[] primeSieve;
-    
-    public Problem46() {
-        primeSieve = new boolean[MAX+1];
-        primeSieve[2] = true;
-        primeSieve[3] = true;
-    }
-    
-    private void fillSieve() {
-        for (int n = 4; n <= MAX; n++) {
-            // Assume it's prime
-            primeSieve[n] = true;
-            
-            for (int m = 2; m < n; m++) {
-                if (n % m == 0) {
-                    primeSieve[n] = false;
-                    break;
-                }
-            }
-        }
-    }
-    
-    public void solve() {
-        fillSieve();
+    public int solve() {
+        final int MAX = 10000;
+        sieve = new SieveOfEratosthenes(MAX);
+        sieve.generate(true);
         
-        for (int n = 9; n <= MAX; n += 2) {
-            if (primeSieve[n])
+        List<Integer> primes = sieve.getPrimes();
+        
+        for (int n = 9; n < MAX; n += 2) {
+            if (sieve.isPrime(n))
                 continue;
-            if (!isGoldbachian(n)) {
-                System.out.println(n);
-                return;
+            
+            for (int prime : primes) {
+                if (prime > n) 
+                    return n;
+                
+                if (isPerfectSquare((n - prime)/2))
+                    break;
             }
         }
-    }
-    
-    public boolean isGoldbachian(int n) {
-        for (int m = 2; m < n; m++) {
-            if (!primeSieve[m])
-                continue;
-            int diff = n - m;
-            if (diff % 2 != 0)
-                continue;
-            int x = diff / 2;
-            if (isPerfectSquare(x)) 
-                return true;
-            
-        }
-        return false;   
+        return 0;
     }
     
     public boolean isPerfectSquare(int x) {
         int sqrt = (int) Math.sqrt(x);
-        if (sqrt * sqrt == x)
-            return true;
-        return false;
+        
+        return sqrt * sqrt == x;
     }
     
     public static void main(String[] args) {
-        new Problem46().solve();
+        int solution = new Problem46().solve();
+        
+        System.out.println("Solution: " + solution);
     }
 }
