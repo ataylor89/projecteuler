4899b20d3f916dcb69e9a067cd22c32cd302dcfc
Andrew Taylor <rolyata47@gmail.com>
2015-03-27 10:58:46 -0500

    Problem 43

--- src/problems/Problem43.java
+++ src/problems/Problem43.java
@@ -3,6 +3,7 @@ package problems;
 import archived.Library;
 import java.util.ArrayList;
 import java.util.List;
+import util.Permutations;
 import util.Sets;
 
 /**
@@ -11,35 +12,47 @@ import util.Sets;
  */
 public class Problem43 {
     
+    int[] primes = new int[] {2,3,5,7,11,13,17};
+    
     public void solve() {
-        List<Long> suchNumbers = new ArrayList<>();
-        List<String> permutations = Sets.permutations("0123456789");
+        List<String> permutations = filter(new Permutations("0123456789").get());
         
-        Library.generatePrimes(1000);
+        List<Long> solutions = new ArrayList<>();
         
         for (String p : permutations) {
             boolean hasProperty = true;
             
-            for (int i = 1; i < 8; i++) {
-                String substr = p.substring(i, i+3);
-                Long subint = Long.parseLong(substr);
+            for (int n = 0; n < primes.length; n++) {
+                int term = Integer.parseInt(p.substring(n+1, n+4));
                 
-                if (subint % Library.getNthPrime(i) != 0) {
+                if (term % primes[n] != 0) {
                     hasProperty = false;
                     break;
                 }
             }
-            if (hasProperty) {
-                suchNumbers.add(Long.parseLong(p));
-            }
+            
+            if (hasProperty)
+                solutions.add(Long.parseLong(p));
         }
-        
-        System.out.println(suchNumbers);
-        
+        System.out.println("Sum of all solutions: " + sum(solutions));
+    }
+    
+    private long sum(List<Long> solutions) {
         long sum = 0;
-        for (long n : suchNumbers) 
-            sum += n;
-        System.out.println("Sum: " + sum);
+        for (long s : solutions)
+            sum += s;
+        
+        return sum;
+    }
+    
+    private List<String> filter(List<String> permutations) {
+        List<String> filtered = new ArrayList<>();
+        
+        for (String p : permutations)
+            if (!p.startsWith("0"))
+                filtered.add(p);
+        
+        return filtered;
     }
     
     public static void main(String[] args) {
