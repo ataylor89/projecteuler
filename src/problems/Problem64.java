219a44294e833b8ffacc722b3131328ca395c822
Andrew Taylor <rolyata47@gmail.com>
2015-04-02 19:16:39 -0500

    Style

--- src/problems/Problem64.java
+++ src/problems/Problem64.java
@@ -10,16 +10,16 @@ public class Problem64 {
     
     public void solve() {
         int upperLimit = 10000;
-        int num = 0;
+        int count = 0;
         
         for (int n = 2; n <= upperLimit; n++) {
             SquareRoot sqrtN = new SquareRoot(n);
             
             if (sqrtN.period() % 2 == 1)
-                num++;
+                count++;
         }
         
-        System.out.println("Continued fractions for N <= " + upperLimit + " with odd period: " + num);
+        System.out.println("Continued fractions for N <= " + upperLimit + " with odd period: " + count);
     }
     
     public static void main(String[] args) {
