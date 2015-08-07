6a717005ef7177c39b7b97a3c21398c6d5b86408
ANDREW TAYLOR <andrewtaylor@wireless-s1-no-150-25-37.uchicago.edu>
2015-08-07 15:40:33 -0500

    n/a

--- src/util/AdjacencyMatrix.java
+++ src/util/AdjacencyMatrix.java
@@ -9,27 +9,27 @@ import java.util.List;
 public class AdjacencyMatrix<T> implements Graph<T> {
 
     @Override
-    public void addEdge(Object n1, Object n2) {
+    public void addEdge(T n1, T n2) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public void removeEdge(Object n1, Object n2) {
+    public void removeEdge(T n1, T n2) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public void hasEdge(Object n1, Object n2) {
+    public void hasEdge(T n1, T n2) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public List outEdges(Object n) {
+    public List<T> outEdges(T n) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public List inEdges(Object n) {
+    public List<T> inEdges(T n) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
